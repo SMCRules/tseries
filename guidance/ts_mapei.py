41204bbe2c7ed296347de690ef7ad6d62fc2fc59
SMCRules <magbelmonte@googlemail.com>
2025-04-26 20:11:55 +0100

    adding SARIMA and comparison table

--- guidance/ts_mapei.py
+++ guidance/ts_mapei.py
@@ -10,8 +10,7 @@ from sklearn.metrics import median_absolute_error, mean_squared_error, mean_squa
 from scipy.optimize import minimize
 import statsmodels.tsa.api as smt
 import statsmodels.api as sm
-
-# from tqdm import tqdm_notebook
+from tqdm import tqdm
 
 from itertools import product
 
@@ -80,7 +79,6 @@ plot_moving_average(data.CLOSE, window=90, plot_intervals=True)
 
 
 ## Exponential smoothing
-
 def exponential_smoothing(series, alpha):
 
     result = [series.iloc[0]] # first value is same as series
@@ -101,6 +99,199 @@ def plot_exponential_smoothing(series, alphas):
     plt.show()
 
 plot_exponential_smoothing(data.CLOSE, [0.05, 0.3])
+
+
+# Double exponential smoothing
+def double_exponential_smoothing(series, alpha, beta):
+
+    result = [series.iloc[0]]
+    for n in range(1, len(series)+1):
+        if n == 1:
+            level, trend = series.iloc[0], series.iloc[1] - series.iloc[0]
+        if n >= len(series): # forecasting
+            value = result[-1]
+        else:
+            value = series.iloc[n]
+        last_level, level = level, alpha * value + (1 - alpha) * (level + trend)
+        trend = beta * (level - last_level) + (1 - beta) * trend
+        result.append(level + trend)
+    return result
+
+def plot_double_exponential_smoothing(series, alphas, betas):
+     
+    plt.figure(figsize=(17, 8))
+    for alpha in alphas:
+        for beta in betas:
+            plt.plot(double_exponential_smoothing(series, alpha, beta), label="Alpha {}, beta {}".format(alpha, beta))
+    plt.plot(series.values, label = "Actual")
+    plt.legend(loc="best")
+    plt.axis('tight')
+    plt.title("Double Exponential Smoothing")
+    plt.grid(True)
+    plt.show()
+
+plot_double_exponential_smoothing(data.CLOSE, alphas=[0.9, 0.02], betas=[0.9, 0.02])
+
+
+# Stationarity
+def tsplot(y, lags=None, figsize=(17, 8), syle='bmh'):
+    
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+        
+    with plt.style.context(style='bmh'):
+        fig = plt.figure(figsize=figsize)
+        layout = (2,2)
+        ts_ax = plt.subplot2grid(layout, (0,0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1,0))
+        pacf_ax = plt.subplot2grid(layout, (1,1))
+        
+        y.plot(ax=ts_ax)
+        p_value = sm.tsa.stattools.adfuller(y)[1]
+        ts_ax.set_title('Time Series Analysis Plots\n Dickey-Fuller: p={0:.5f}'.format(p_value))
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax)
+        plt.tight_layout()
+        plt.show()
+        
+tsplot(data.CLOSE, lags=30)
+
+# first difference
+data_diff = data.CLOSE - data.CLOSE.shift(1)
+tsplot(data_diff[1:], lags=30)
+
+### SARIMA
+#Set initial values and some bounds
+ps = range(0, 5)
+d = 1
+qs = range(0, 5)
+Ps = range(0, 5)
+D = 1
+Qs = range(0, 5)
+s = 5
+
+#Create a list with all possible combinations of parameters
+parameters = product(ps, qs, Ps, Qs)
+parameters_list = list(parameters)
+print('Number of parameters:', len(parameters_list))
+
+def optimize_SARIMA(parameters_list, d, D, s, data):
+    """
+    Return dataframe with parameters and corresponding AIC.
+    
+    parameters_list - list with (p, q, P, Q) tuples
+    d - integration order
+    D - seasonal integration order
+    s - length of season
+    data - dataframe with a 'CLOSE' column
+    """
+    
+    # --- PREPROCESSING ---
+    data = data.sort_index()
+    data.index = pd.DatetimeIndex(data.index)
+    try:
+        data = data.asfreq(pd.infer_freq(data.index))
+    except:
+        pass  # If can't infer, just proceed without frequency
+    
+    results = []
+    best_aic = float('inf')
+    
+    for param in tqdm(parameters_list):
+        try:
+            model = sm.tsa.statespace.SARIMAX(
+                data.CLOSE, 
+                order=(param[0], d, param[1]),
+                seasonal_order=(param[2], D, param[3], s)
+            ).fit(disp=False)
+        except Exception as e:
+            print(f"Failed to fit SARIMA{param} - {e}")
+            continue
+        
+        aic = model.aic
+        
+        if aic < best_aic:
+            best_model = model
+            best_aic = aic
+            best_param = param
+        
+        results.append([param, model.aic])
+        
+    result_table = pd.DataFrame(results)
+    result_table.columns = ['parameters', 'aic']
+    result_table = result_table.sort_values(by='aic', ascending=True).reset_index(drop=True)
+    
+    return result_table
+
+
+
+result_table = optimize_SARIMA(parameters_list, d, D, s, data)
+
+HBox(children=(IntProgress(value=0, max=625), HTML(value='')))
+
+#Set parameters that give the lowest AIC (Akaike Information Criteria)
+
+p, q, P, Q = result_table.parameters[0]
+
+best_model = sm.tsa.statespace.SARIMAX(data.CLOSE, order=(p, d, q),
+                                       seasonal_order=(P, D, Q, s)).fit(disp=-1)
+
+print(best_model.summary())
+
+def plot_SARIMA(series, model, n_steps):
+    """
+        Plot model vs predicted values
+        
+        series - dataset with time series
+        model - fitted SARIMA model
+        n_steps - number of steps to predict in the future
+    """
+    
+    data = series.copy().rename(columns = {'CLOSE': 'actual'})
+    data['arima_model'] = model.fittedvalues
+    #Make a shift on s+d steps, because these values were unobserved by the model due to the differentiating
+    data['arima_model'][:s+d] = np.NaN
+    
+    #Forecast on n_steps forward
+    forecast = model.predict(start=data.shape[0], end=data.shape[0] + n_steps)
+    forecast = data.arima_model.append(forecast)
+    #Calculate error
+    error = mean_absolute_percentage_error(data['actual'][s+d:], data['arima_model'][s+d:])
+    
+    plt.figure(figsize=(17, 8))
+    plt.title('Mean Absolute Percentage Error: {0:.2f}%'.format(error))
+    plt.plot(forecast, color='r', label='model')
+    plt.axvspan(data.index[-1], forecast.index[-1],alpha=0.5, color='lightgrey')
+    plt.plot(data, label='actual')
+    plt.legend()
+    plt.grid(True)
+    plt.show();
+    
+# plot_SARIMA(data, best_model, 5)
+print(best_model.predict(start=data.CLOSE.shape[0], end=data.CLOSE.shape[0] + 5))
+print(mean_absolute_percentage_error(data.CLOSE[s+d:], best_model.fittedvalues[s+d:]))
+
+comparison = pd.DataFrame({'actual': [18.93, 19.23, 19.08, 19.17, 19.11, 19.12],
+                          'predicted': [18.96, 18.97, 18.96, 18.92, 18.94, 18.92]}, 
+                          index = pd.date_range(start='2018-06-05', periods=6,))
+
+print(comparison.head())
+
+plt.figure(figsize=(17, 8))
+plt.plot(comparison.actual)
+plt.plot(comparison.predicted)
+plt.title('Predicted closing price of New Germany Fund Inc (GF)')
+plt.ylabel('Closing price ($)')
+plt.xlabel('Trading day')
+plt.legend(loc='best')
+plt.grid(False)
+plt.show()
+
+
+
+
+
+
 
 
 
