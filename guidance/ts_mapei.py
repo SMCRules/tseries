901e0b57284c54f92fff86f8a3f461903ccf51ce
SMCRules <magbelmonte@googlemail.com>
2025-04-26 18:39:35 +0100

    adding EDA

--- guidance/ts_mapei.py
+++ guidance/ts_mapei.py
@@ -20,6 +20,32 @@ def mean_absolute_percentage_error(y_true, y_pred):
 
 path = '/home/miguel/Python_Projects/datasets/stock_prices_sample.csv'
 data = pd.read_csv(path, index_col=['DATE'], parse_dates=['DATE'])
+# print(data.head(10))
+# print(data.shape)
+# print(data.dtypes)
+
+"""
+Concentrate on the New Germany Fund (GF) and the end of day (EOD) information.
+Remove unwanted columns, to focus on the stocks closing price.
+"""
+data = data[data.TICKER != 'GEF']
+data = data[data.TYPE != 'Intraday']
+drop_cols = [
+    'SPLIT_RATIO', 'EX_DIVIDEND', 'ADJ_FACTOR', 
+    'ADJ_VOLUME', 'ADJ_CLOSE', 'ADJ_LOW', 'ADJ_HIGH', 
+    'ADJ_OPEN', 'VOLUME', 'FREQUENCY', 'TYPE', 'FIGI'
+    ]
+data.drop(drop_cols, axis=1, inplace=True)
 print(data.head(10))
-print(data.shape)
-print(data.dtypes)
+
+# Exploratory data analysis (EDA)
+
+plt.figure(figsize=(17, 8))
+plt.plot(data.CLOSE)
+plt.title('Closing price of New Germany Fund Inc (GF)')
+plt.ylabel('Closing price ($)')
+plt.xlabel('Trading day')
+plt.grid(False)
+plt.show()
+
+
